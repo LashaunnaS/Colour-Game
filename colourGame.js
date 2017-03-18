fc0ece32a81cde7e3e88f5b9f41bad10247576de
Lashaunna Samuels <shaunna191@hotmail.com>
2017-03-18 15:20:04 -0400

    Reduced the amount of lines of code by cleaning up longer methods

--- colourGame.js
+++ colourGame.js
@@ -6,54 +6,41 @@ var colourDisplay = document.getElementById("colourDisplay");
 var msgDisplay = document.querySelector("#msg");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
+for (var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function() {
+        modeButtons[0].classList.remove("selected");
+        modeButtons[1].classList.remove("selected");
+        this.classList.add("selected");
+        this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+        reset();
+    })
+}
 
-easyBtn.addEventListener("click", function() {
-    hardBtn.classList.remove("selected");
-    easyBtn.classList.add("selected");
-    numSquares = 3;
-    colours = generateRandomColours(numSquares);
-    pickedColour = pickColour();
-    colourDisplay.textContent = pickedColour;
-    for (var i = 0; i < squares.length; i++) {
-        if (colours[i]) {
-            squares[i].style.background = colours[i];
-        } else {
-            squares[i].style.display = "none";
-        }
-    }
-});
-
-hardBtn.addEventListener("click", function() {
-    hardBtn.classList.add("selected");
-    easyBtn.classList.remove("selected");
-    numSquares = 6;
-    colours = generateRandomColours(numSquares);
-    pickedColour = pickColour();
-    colourDisplay.textContent = pickedColour;
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].style.background = colours[i];
-        squares[i].style.display = "block";
-    }
-});
-
-resetButton.addEventListener("click", function() {
+function reset() {
     // generate all new colours
     colours = generateRandomColours(numSquares);
     //pick a new random colour from array
     pickedColour = pickColour();
     //change colours of squares
     colourDisplay.textContent = pickedColour;
-    this.textContent = "New Colours";
-
+    resetButton.textContent = "New Colours";
     msgDisplay.textContent = "";
     //change colours to squares
     for (var i = 0; i < squares.length; i++) {
-        squares[i].style.background = colours[i];
+      if (colours[i]) {
+         squares[i].style.display = "block";
+         squares[i].style.background = colours[i];
+      } else {
+        squares[i].style.display = "none";
+      }
     }
     h1.style.background = "steelblue";
+}
+
+resetButton.addEventListener("click", function() {
+   reset();
 })
 
 colourDisplay.textContent = pickedColour;
